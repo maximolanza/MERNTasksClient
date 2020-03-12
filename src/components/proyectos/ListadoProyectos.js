fae3bd1877a7c3309a45d31cf61d0dd4de9d5ed4
Máximo <max.slanza@gmail.com>
2020-03-12 16:25:56 -0300

    Proyectos en State

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Fragment, useState, useContext } from 'react';
 import Proyecto from './Proyecto';
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
 
 
 const ListadoProyectos = () => {
 
-    const proyectos = [
-        { nombre: 'Tienda Virtual'},
-        {nombre: 'Intranet'},
-        {nombre: 'Diseño de Sitio web'}
-    ]
-    return ( 
+    // Extraer proyectos de state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const { proyectos, obtenerProyectos } = proyectosContext;
+
+    // Obtener proyectos
+    useEffect(() => {
+        obtenerProyectos();
+    }, [])
+
+
+
+    //Revisar si proyectos tiene contenido
+        if(proyectos.length === 0) return null;
+
+       
+    return (  
         <ul className="listado-proyectos">
             { proyectos.map( proyecto => (
                 <Proyecto 
+                    key = { proyecto.id }
                     proyecto={ proyecto }/>
             ))}
         </ul>
